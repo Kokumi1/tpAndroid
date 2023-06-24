f295c167fd8c9b977631dd4dfd064d62df0c9985
Naplucia <fldebruyck@gmail.com>
2023-06-24 14:35:43 +0200

    add spacer

--- app/src/main/java/com/example/tpandroid/ui/theme/screen/login.kt
+++ app/src/main/java/com/example/tpandroid/ui/theme/screen/login.kt
@@ -3,6 +3,7 @@ package com.example.tpandroid.ui.theme.screen
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -12,9 +13,11 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -32,42 +35,34 @@ fun Login() {
     Scaffold {
         val context = LocalContext.current
         FirebaseApp.initializeApp(context)
+        val firebaseAuth = Firebase.auth
 
-        Column(modifier = Modifier.padding(it)) {
+        Column(modifier = Modifier.padding(it),
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             TextField(
                 value = emailTextField.value, onValueChange = { email ->
                     emailTextField.value = email
+
                 }, placeholder = { Text(text = "email") }
             )
 
             TextField(value = passwordTextFieldValue.value, onValueChange = { password ->
                 passwordTextFieldValue.value = password
             }, placeholder={ Text(text="password")})
+            Spacer(modifier = Modifier.padding(12.dp))
 
             Button(onClick = {
-                val firebaseAuth = Firebase.auth
+
                 Log.i("Auth", "register begin")
 
-                    /*firebaseAuth.createUserWithEmailAndPassword(
-                    emailTextField.value.text, passwordTextFieldValue.value.text
-                )
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            Log.i("Auth", "login complete!!")
-
-
-                        } else if (task.isCanceled) {
-                            Log.e("Auth", "What are you doing?!")
-                        }
-                    }.addOnFailureListener {
-                        Log.e("Auth", "mission failed!")
-                    }*/
                 firebaseAuth.signInWithEmailAndPassword(
                     emailTextField.value.text, passwordTextFieldValue.value.text)
                     .addOnCompleteListener { task ->
                         if (task.isSuccessful) {
                             // Sign in success, update UI with the signed-in user's information
                            Log.i("AUTH","Login complete")
+
                         } else {
                             // If sign in fails, display a message to the user.
                             Toast.makeText(
@@ -80,10 +75,26 @@ fun Login() {
             }) {
                 Text(text = "login")
             }
+            Spacer(modifier = Modifier.padding(20.dp))
 
+            Button(onClick={
+                firebaseAuth.createUserWithEmailAndPassword(
+                    emailTextField.value.text, passwordTextFieldValue.value.text
+                )
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.i("Auth", "register complete!!")
+
+                        } else if (task.isCanceled) {
+                            Log.e("Auth", "What are you doing?!")
+                        }
+                    }.addOnFailureListener {
+                        Log.e("Auth", "mission failed!")
+                    }
+            }){
+                Text(text = "register")
+            }
 
         }
-
-
     }
 }
